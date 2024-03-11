33712eb041cf5ff4f4ab8362b3cabf3087781a1c
danilo.silva <danilo.silva@msn.com>
2024-03-10 23:37:58 -0300

    Criando uma Nova Migration

---
+++ Skinet/Skinet.Infrastructure/Data/Migrations/20240311014909_InitialCreate.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Skinet.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MarcasProduto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MarcasProduto", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TiposProduto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TiposProduto", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: true),
+                    Preco = table.Column<decimal>(type: "TEXT", nullable: false),
+                    ImagemUrl = table.Column<string>(type: "TEXT", nullable: true),
+                    TipoProdutoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    MarcaProdutoId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Produtos_MarcasProduto_MarcaProdutoId",
+                        column: x => x.MarcaProdutoId,
+                        principalTable: "MarcasProduto",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Produtos_TiposProduto_TipoProdutoId",
+                        column: x => x.TipoProdutoId,
+                        principalTable: "TiposProduto",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_MarcaProdutoId",
+                table: "Produtos",
+                column: "MarcaProdutoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_TipoProdutoId",
+                table: "Produtos",
+                column: "TipoProdutoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "MarcasProduto");
+
+            migrationBuilder.DropTable(
+                name: "TiposProduto");
+        }
+    }
+}
